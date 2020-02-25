44ba1f5fed2c1b9868c55d38059aa3b28660efb1
hezhuangzhuang <hezhuangzhuang@cecdat.com>
2020-02-25 09:09:22 +0800

    '微服务之间调用openfEIGN'

--- gmall-item/leyou-item-service/src/main/java/com/gmall/item/controller/Test.java
+++ gmall-item/leyou-item-service/src/main/java/com/gmall/item/controller/Test.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 拼接sql测试类
+ */
 @RestController
 public class Test {
     @Autowired
@@ -15,4 +21,29 @@ public class Test {
         Object catalog1 = testService.getCatalog1();
         return catalog1;
     }
+
+    public static void main(String[] args) {
+        List<String> orgs = Arrays.asList("SELECT", "medical_org_name", "from"
+                , "t_dataset_level_hos_table_num", "GROUP", "BY", "medical_org_name", "LIMIT",
+                "0", ",", "10", ";");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String sql = null;
+
+        stringBuilder.append("SELECT\n" +
+                "b.dataset_table_num as total,\n" +
+                "a.dataset_struc_id \n");
+        for (String org : orgs) {
+            sql = String.format("SUM(CASE `medical_org_name` WHEN '罗源县医院' THEN dataset_hos_table_num ELSE 0 END) AS '罗源县医院', \n",
+            org);
+            stringBuilder.append(sql);
+
+        }
+        stringBuilder.append("a.dataset_struc_desc \n" + "FROM\n" +
+                " t_dataset_level_hos_table_num  a left join t_dataset_level_table_num b on a.dataset_struc_id =b.dataset_struct_id\n" +
+                "\n" +
+                "GROUP BY a.dataset_struc_id;\n");
+        System.out.println(stringBuilder.toString());
+    }
 }
+
--- gmall-item/leyou-item-service/src/main/java/com/gmall/item/service/ItemClient.java
+++ gmall-item/leyou-item-service/src/main/java/com/gmall/item/service/ItemClient.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * openFeign的使用规则
+ * @FeignClient(name = "manage-service") name为被调用服务名
+ * @RequestMapping("/searchPmsSkuInfoMsg") 被调用服务的请求路径
+ * 返回值必须和被调用服务的返回值相同
+ */
 @FeignClient(name = "manage-service")
 public interface ItemClient {
     @RequestMapping("/searchPmsSkuInfoMsg")
